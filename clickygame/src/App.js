4a28d5779852ddede7e7f5d5672c23c60ceb22e3
Jessica Son <jessicason18@gmail.com>
2018-07-12 00:18:04 -0700

    img partly working

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -3,13 +3,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import Image from "./components/Image/Image"
 import './App.css';
+import images from "./images.json";
 
 
 const App = () => (
   <div className="container">
     <Navbar/>
     <Jumbotron/>
-    <Image/> 
+    <Image
+     image={images[0].image}
+     /> 
   </div>
 );
 
--- clickygame/src/components/Image/Image.js
+++ clickygame/src/components/Image/Image.js
@@ -1,12 +1,26 @@
 import React from "react";
-import "./Image.css"
-import "./Imgs"
+import "./Image.css";
 
-const Image = () => (
-   <div className = "image">
+// import Dog-10 from '../../Imgs/Dog-10.jpg';
+// import Dog-11 from '../../Imgs/Dog-11.jpg';
+// import Dog-1 from '../../Imgs/Dog-1.jpeg';
+// import Dog-2 from '../../Imgs/Dog-2.jpg';
+// import Dog-3 from '../../Imgs/Dog-3.jpg'';
+// import Dog-4 from '../../Imgs/Dog-4.jpg'';
+// import Dog-5 from '../../Imgs/Dog-5.jpg'';
+// import Dog-6 from '../../Imgs/Dog-6.jpeg'';
+// import Dog-7 from '../../Imgs/Dog-7.jpg'';
+// import Dog-8 from '../../Imgs/Dog-8.jpeg'';
+// import Dog-9 from '../../Imgs/Dog-9.jpg'';
+// import Dog-12 from '../../Imgs/Dog-12.jpg'';
 
-   </div>
-);
+const Image = props => (
+    <div className="image">
+      <div className="img-container">
+        <img src={props.image} />
+      </div>
+    </div>
+  );
 
 export default Image;
 
