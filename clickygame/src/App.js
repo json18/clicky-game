1b8a25294048734f7697b56e4bb2e2b3a7c368ce
Jessica Son <jessicason18@gmail.com>
2018-07-12 18:27:18 -0700

    Added

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import Image from "./components/Image/Image"
@@ -6,50 +6,70 @@ import './App.css';
 import images from "./images.json";
 
 
-const App = () => (
+class App extends Component {
+  constructor () {
+    super();
+  }
+
+
+state = {
+  images
+};
+
+render () { 
+  return (
   <div className="container">
     <Navbar/>
     <Jumbotron/>
+
+  {this.state.images.map(image => (
     <Image
-     image={images[0].image}
-     /> 
-       <Image
-     image={images[1].image}
-     /> 
-       <Image
-     image={images[2].image}
-     /> 
-       <Image
-     image={images[3].image}
-     /> 
-       <Image
-     image={images[4].image}
-     /> 
-       <Image
-     image={images[5].image}
-     /> 
-       <Image
-     image={images[6].image}
-     /> 
-       <Image
-     image={images[7].image}
-     /> 
-       <Image
-     image={images[8].image}
-     /> 
-       <Image
-     image={images[9].image}
-     /> 
-       <Image
-     image={images[10].image}
-     /> 
-       <Image
-     image={images[11].image}
-     /> 
-       {/* <Image
-     image={images[12].image}
-     />  */}
-  </div>
-);
+     image={images.image}
+     key={images.id}
+     onClick={() => {
+        console.log("this is working")
+     }}
+     />
+    ))};
+    </div>
+    //    <Image
+    //  image={images[1].image}
+    //  /> 
+    //    <Image
+    //  image={images[2].image}
+    //  /> 
+    //    <Image
+    //  image={images[3].image}
+    //  /> 
+    //    <Image
+    //  image={images[4].image}
+    //  /> 
+    //    <Image
+    //  image={images[5].image}
+    //  /> 
+    //    <Image
+    //  image={images[6].image}
+    //  /> 
+    //    <Image
+    //  image={images[7].image}
+    //  /> 
+    //    <Image
+    //  image={images[8].image}
+    //  /> 
+    //    <Image
+    //  image={images[9].image}
+    //  /> 
+    //    <Image
+    //  image={images[10].image}
+    //  /> 
+    //    <Image
+    //  image={images[11].image}
+    //  /> 
+
+  )}
+  
+  }
 
 export default App;
+
+
--- clickygame/src/components/Image/Image.js
+++ clickygame/src/components/Image/Image.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "./Image.css";
 
-// import Dog-10 from '../../Imgs/Dog-10.jpg';
-// import Dog-11 from '../../Imgs/Dog-11.jpg';
-// import Dog-1 from '../../Imgs/Dog-1.jpeg';
-// import Dog-2 from '../../Imgs/Dog-2.jpg';
-// import Dog-3 from '../../Imgs/Dog-3.jpg'';
-// import Dog-4 from '../../Imgs/Dog-4.jpg'';
-// import Dog-5 from '../../Imgs/Dog-5.jpg'';
-// import Dog-6 from '../../Imgs/Dog-6.jpeg'';
-// import Dog-7 from '../../Imgs/Dog-7.jpg'';
-// import Dog-8 from '../../Imgs/Dog-8.jpeg'';
-// import Dog-9 from '../../Imgs/Dog-9.jpg'';
-// import Dog-12 from '../../Imgs/Dog-12.jpg'';
+class Image extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-const Image = props => (
-    <div className="image">
-      <div className="img-container">
-        <img className="imageClass" alt="" src={props.image} />
-      </div>
-    </div>
-  );
+
+    render() {
+
+        return (
+            <div>
+                <div className="image">
+                    <div className="img-container">
+                        <img className="imageClass" alt="" src={this.props.image} onClick={this.props.onClick} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Image;
 
-  
